68b01ba6b8db0b68bc09c3942e87ccb302dba1c4
player_eric <digimonyan@gmail.com>
2024-08-07 19:24:11 -0400

    fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     remix({
+      basename: "/portfolio-in-10-mins",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
